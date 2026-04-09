b624e5490b78cad2964f3e01e8efcaae98d0ba4f
BaHaMuT11 <bahamut@bahatech.cl>
2026-04-08 23:26:14 -0400

    SOLICITUD 2: Se reestablece anteposición de SRCEI para oficinas y CONSULADO para consulado en campo oficina

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -75,8 +75,8 @@ const TicketForm = () => {
                             formTransform(correo));
         const construirMaquina = () => (formTransform(formTicket.tipoMaquina) === "SI" ? "S/I" :formTransform(formTicket.tipoMaquina) + "-" + formTransform(formTicket.maquina));
         const construirOficina = () => (
-            formTransform(formTicket.tipoOficina) === "OFICINA"  ? ""
-                + formTransform(traducirOficina(formTicket.oficina)) : "" + formTransform(traducirOficina(formTicket.oficina)));
+            formTransform(formTicket.tipoOficina) === "OFICINA"  ? "SRCEI "
+                + formTransform(traducirOficina(formTicket.oficina)) : "CONSULADO " + formTransform(traducirOficina(formTicket.oficina)));
 
         setCorreoExport(construirCorreo());
         setMaquinaExport(construirMaquina());
